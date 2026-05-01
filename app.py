3837c8be6864fe032dc6ee82a652456a57fbf081
Vishal <simt05nov@gmail.com>
2026-05-01 14:11:00 +0530

    Changes has been closed but not merged

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ def get_connection(server):
 # Added a feature branch
 @app.route("/")
 def home():
-    return "Flask Active-Active DB App Running! pushed by feature branch"
+    return "Flask Active-Active DB App Running! pushed by feature branch previous was closed"
 
 # 🔹 Create table (run once)
 @app.route("/init")
